c8503cd34e6dc757c95462ffd4baee3aeecdd44d
yangtiancheng <yangtiancheng@lixiang.com>
2022-07-31 21:38:27 +0800

    dev_suanfa commit 1.0.6

--- src/com/crx/suanfa/Paixu.java
+++ src/com/crx/suanfa/Paixu.java
@@ -64,11 +64,6 @@ public class Paixu {
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
             adjustHeap(arr, i, arr.length);
         }
-        /**
-         * 将堆项元素与末尾元素交换，将最大元素"沉"到数组末端;
-         * 重新调整结构，使其满足堆定义，然后继续交换堆项元素与当前末尾元素，反复执行调整+交换步骤，直到整个序列有序。
-         */
-
         for (int j = arr.length - 1; j > 0; j--) {
             temp = arr[j];
             arr[j] = arr[0];
@@ -81,16 +76,6 @@ public class Paixu {
     }
 
 
-    /**
-     * 将一个数组（二叉树）调整成一个大根堆
-     * 功能：完成将以i对应的非叶子结点的树调整成大顶堆
-     * 举例int arr[]={4, 6,8,5,9};=>i=1=> adjustHeap=>得到{4,9,8,5, 6}
-     * 如果我们再次调用adjustHeap 传入的是i=0=>得到{4,9, 8,5,6}=> {9,6,8,5, 4}
-     *
-     * @param arr    待调整的数组
-     * @param i      表示非叶子结点在数组中索引
-     * @param length 表示对多少个元素继续调整，length 是在逐渐的减少
-     */
     public static void adjustHeap(int arr[], int i, int length) {
 
         int temp = arr[i];//先取出当前元素的值，保存在临时变量
